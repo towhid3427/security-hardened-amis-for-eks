6cc48352eb533c0e9f8e7374a8e30f3eacfd8fca
Maicon Alegre <alegrm@amazon.com>
2025-08-21 13:15:57 +0930

    remove additional policies as it is being added on the managed node groups creation

--- patterns/modules/eks-cluster/main.tf
+++ patterns/modules/eks-cluster/main.tf
@@ -25,20 +25,7 @@ module "eks" {
 
   vpc_id = data.aws_ssm_parameter.vpc_id.value
 
-  #subnet_ids =  data.aws_ssm_parameter.private_subnets.value
   subnet_ids = tolist(split(",", data.aws_ssm_parameter.private_subnets.value))
-
-## Causing an error like below
-##   # expected length of name_prefix to be in the range (1 - 38), got .........
-##  https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2053#issuecomment-1317721424
-## This Additional IAM Policy is already configured individually while creating each NodeGroup.
-  eks_managed_node_groups = {
-    iam_role_additional_policies = {
-      # Not required, but used in the example to access the nodes to inspect mounted volumes
-      AmazonSSMManagedInstanceCore     = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-      AmazonInspector2ManagedCisPolicy = "arn:aws:iam::aws:policy/AmazonInspector2ManagedCisPolicy"
-    }
-  }
 
   tags = var.tags
 
