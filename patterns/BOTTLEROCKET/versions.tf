e1a564407d0d547de595bc7b11e1e592c717291c
Maicon Alegre <alegrm@amazon.com>
2025-09-29 15:40:06 +0930

    rollback bucket name

--- patterns/BOTTLEROCKET/versions.tf
+++ patterns/BOTTLEROCKET/versions.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-backend-eks-cis-us-west-2"
+    bucket  = "BUCKET"
     key     = "BOTTLEROCKET"
     region  = "us-west-2"
     encrypt = true
