bde796e5e3301c158b039d782be2d6765d04b957
towhid3427 <towhidul@amazon.com>
2025-12-03 10:50:50 +1100

    Removing CIS_AL2 and EKS_Optimized_AL2

--- patterns/CIS_AL2/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "BUCKET_NAME"
-    key     = "CIS_AL2"
-    region  = "us-west-2"
-    encrypt = true
-  }
-}
--- patterns/EKS_Optimized_AL2/versions.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "BUCKET_NAME"
-    key     = "EKS_Optimized_AL2"
-    region  = "us-west-2"
-    encrypt = true
-  }
-}
